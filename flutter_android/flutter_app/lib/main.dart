c8bac1d0a09b8d5ccbb3c5e3f7ac682c376529c7
tjrrms0809 <tjrrms0809@github.com>
2020-05-01 18:27:43 +0900

    123

---
+++ flutter_android/flutter_app/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Container',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.symmetric(
+            vertical: 50,
+            horizontal: 10
+          ),
+          padding: EdgeInsets.all(20),
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
+
